faa78b8f50276bb808546bf79c9e6f9e39b1adef
Joe Bramhall <joe@thinxmedia.com>
2019-12-31 07:45:12 -0500

    Initial project setup

---
+++ SwapiSwift/SwapiSwift/SwapiSwift.h
@@ -0,0 +1,19 @@
+//
+//  SwapiSwift.h
+//  SwapiSwift
+//
+//  Created by Joe Bramhall on 12/31/19.
+//  Copyright © 2019 thinx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwapiSwift.
+FOUNDATION_EXPORT double SwapiSwiftVersionNumber;
+
+//! Project version string for SwapiSwift.
+FOUNDATION_EXPORT const unsigned char SwapiSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwapiSwift/PublicHeader.h>
+
+
