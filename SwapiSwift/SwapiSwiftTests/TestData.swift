a56c61458b35db1f36e98d0195058aff910203e9
Joe Bramhall <joe@thinxmedia.com>
2020-01-01 15:25:18 -0500

    Moved

--- SwapiSwift/SwapiSwiftTests/TestData.swift
+++ SwapiSwift/SwapiSwiftTests/TestData.swift
@@ -9,26 +9,6 @@
 import Foundation
 @testable import SwapiSwift
 
-extension Formatter {
-  static let iso8601: ISO8601DateFormatter = {
-    let formatter = ISO8601DateFormatter()
-    formatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
-    return formatter
-  }()
-  static let iso8601NoFs = ISO8601DateFormatter()
-}
-
-extension JSONDecoder.DateDecodingStrategy {
-  static let customISO8601 = custom {
-    let container = try $0.singleValueContainer()
-    let string = try container.decode(String.self)
-    if let date = Formatter.iso8601.date(from: string) ?? Formatter.iso8601NoFs.date(from: string) {
-      return date
-    }
-    throw DecodingError.dataCorruptedError(in: container, debugDescription: "Invalid date format \(string)")
-  }
-}
-
 var sampleFilm: Film {
   load("film_response")
 }
