ca51dcf031161eb7327700a43c9fd0a9ea0dc332
Joe Bramhall <joe@thinxmedia.com>
2020-01-01 15:24:55 -0500

    Added extensions for successful parsing

--- SwapiSwift/SwapiSwift/Parsing.swift
+++ SwapiSwift/SwapiSwift/Parsing.swift
@@ -9,10 +9,30 @@
 import Foundation
 import Combine
 
+extension Formatter {
+  static let iso8601: ISO8601DateFormatter = {
+    let formatter = ISO8601DateFormatter()
+    formatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+    return formatter
+  }()
+  static let iso8601NoFs = ISO8601DateFormatter()
+}
+
+extension JSONDecoder.DateDecodingStrategy {
+  static let customISO8601 = custom {
+    let container = try $0.singleValueContainer()
+    let string = try container.decode(String.self)
+    if let date = Formatter.iso8601.date(from: string) ?? Formatter.iso8601NoFs.date(from: string) {
+      return date
+    }
+    throw DecodingError.dataCorruptedError(in: container, debugDescription: "Invalid date format \(string)")
+  }
+}
+
 /// A free function that decodes a `Data` object to a decodable model and returns a type-erasing publisher. 
 func decode<T: Decodable>(_ data: Data) -> AnyPublisher<T, ServiceError> {
   let decoder = JSONDecoder()
-  decoder.dateDecodingStrategy = .iso8601
+  decoder.dateDecodingStrategy = .customISO8601
 
   return Just(data)
     .decode(type: T.self, decoder: decoder)
