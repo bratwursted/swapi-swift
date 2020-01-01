48328974d70b1512b512428397857363dc582f01
Joe Bramhall <joe@thinxmedia.com>
2020-01-01 14:43:55 -0500

    Updated SWAPI service

--- SwapiSwift/SwapiSwift/SwapiService.swift
+++ SwapiSwift/SwapiSwift/SwapiService.swift
@@ -7,7 +7,37 @@
 //
 
 import Foundation
+import Combine
+
+enum ServiceError: Error {
+  case unknown
+  case networkError(error: NetworkError)
+  case parsingError(error: Error)
+}
 
 protocol SwapiService {
+
+  func film(withId resourceId: String) -> AnyPublisher<Film, ServiceError> 
   
+}
+
+struct DataService: SwapiService {
+
+  private let dataFetcher = DataFetcher()
+
+  func film(withId resourceId: String) -> AnyPublisher<Film, ServiceError> {
+    let config = RequestConfiguration(resource: .films)
+    return swapiResource(request: Request(config: config, resourceId: resourceId))
+  }
+
+  private func swapiResource<T: Decodable>(request: Request) -> AnyPublisher<T, ServiceError> {
+    return dataFetcher.fetch(request.url)
+      .mapError { error in
+        ServiceError.networkError(error: error)
+    }
+    .flatMap(maxPublishers: .max(1)) { data in
+      decode(data)
+    }
+    .eraseToAnyPublisher()
+  }
 }
