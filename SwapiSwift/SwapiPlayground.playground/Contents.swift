1edcec813934ae7c4ff1230aeb43ebaebc49a516
Joe Bramhall <joe@thinxmedia.com>
2020-01-01 15:20:05 -0500

    More playground files

--- SwapiSwift/SwapiPlayground.playground/Contents.swift
+++ SwapiSwift/SwapiPlayground.playground/Contents.swift
@@ -1,3 +1,23 @@
 import UIKit
+import SwapiSwift
+import Combine
 
 var str = "Hello, playground"
+
+let service = DataService()
+
+let disposables = Set<AnyCancellable>()
+
+service.film(withId: "1")
+  .receive(on: DispatchQueue.main)
+  .sink(receiveCompletion: { completion in
+    switch completion {
+    case .finished:
+      break
+    case .failure(let error):
+      print("there was an error \(error)")
+    }
+  }) { film in
+    print("got the film titled \"\(film.title)\"")
+}
+
