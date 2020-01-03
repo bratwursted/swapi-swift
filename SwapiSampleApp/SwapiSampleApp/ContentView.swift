0c19522580782b94d781aa6507d5619bdc374488
Joe Bramhall <joe@thinxmedia.com>
2020-01-03 08:16:27 -0500

    Added sample app to workspace

---
+++ SwapiSampleApp/SwapiSampleApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwapiSampleApp
+//
+//  Created by Joe Bramhall on 1/3/20.
+//  Copyright © 2020 thinx. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
