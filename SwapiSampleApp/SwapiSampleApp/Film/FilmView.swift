459d692b14f0b6cc62819f82a18c7ed8f03d2689
Joe Bramhall <joe@thinxmedia.com>
2020-01-03 16:10:36 -0500

    Added film view and view model

--- SwapiSampleApp/SwapiSampleApp/Film/FilmView.swift
+++ SwapiSampleApp/SwapiSampleApp/Film/FilmView.swift
@@ -7,15 +7,52 @@
 //
 
 import SwiftUI
+import SwapiSwift
 
 struct FilmView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+  let viewModel: FilmViewModel
+
+  var body: some View {
+    List {
+      basicInfoSection
+      productionSection
     }
+    .listStyle(GroupedListStyle())
+    .padding()
+  }
+
+  var basicInfoSection: some View {
+    Section {
+      VStack(alignment: .leading, spacing: 6) {
+        Text(viewModel.episideTitle)
+          .font(.title)
+        Text(viewModel.filmTitle)
+          .font(.body)
+        Text("\"\(viewModel.crawlSnipet)\"")
+          .font(.subheadline)
+      }
+    }
+  }
+
+  var productionSection: some View {
+    Section {
+      VStack(alignment: .leading, spacing: 6) {
+        Text("Released: \(viewModel.formattedReleaseDate)")
+          .font(.callout)
+        Text("Director: \(viewModel.director)")
+        Text("Producers: \(viewModel.producers)")
+      }
+    }
+  }
+
 }
 
 struct FilmView_Previews: PreviewProvider {
-    static var previews: some View {
-        FilmView()
-    }
+  static var previews: some View {
+    let film = sampleFilms.results[0]
+    let dataService = MockGraphDataService(dataStore: newHopeData)
+    let service = FilmGraphService(film: film, dataService: dataService)
+    return FilmView(viewModel: FilmViewModel(filmService: service))
+  }
 }
--- SwapiSampleApp/SwapiSampleApp/Film/FilmViewModel.swift
+++ SwapiSampleApp/SwapiSampleApp/Film/FilmViewModel.swift
@@ -7,3 +7,86 @@
 //
 
 import Foundation
+import SwapiSwift
+import Combine
+
+final class FilmViewModel: ObservableObject {
+
+  private let formatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd"
+    return formatter
+  }()
+
+  private let outputFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .medium
+    return formatter
+  }()
+
+  private let filmService: FilmGraphService
+
+  private var film: Film {
+    filmService.film
+  }
+
+  init(
+    filmService: FilmGraphService
+  ) {
+    self.filmService = filmService
+  }
+
+  func fetchFilmProperties() {
+    filmService.fetchAssociatedProperties()
+  }
+
+  var episideTitle: String {
+    film.title
+  }
+
+  var filmTitle: String {
+    "Episode \(episode)"
+  }
+
+  var formattedReleaseDate: String {
+    outputFormatter.string(from: releaseDate)
+  }
+
+  var director: String {
+    film.director
+  }
+
+  var producers: String {
+    film.producer
+  }
+
+  var crawlSnipet: String {
+    film.openingCrawl.firstSentence ?? "n/a"
+  }
+
+  private var releaseDate: Date {
+    formatter.date(from: film.releaseDate)!
+  }
+
+  private var episode: String {
+    String(film.episodeId)
+  }
+}
+
+private extension String {
+
+  var firstSentence: String? {
+    sentences.first
+  }
+
+  var sentences: [String] {
+    let range = self.startIndex..<self.endIndex
+    var sentences: [String] = []
+    self.enumerateSubstrings(in: range, options: .bySentences) { (substring, range, enclosingRange, stopPointer) in
+      if let sentence = substring {
+        sentences.append(sentence.trimmingCharacters(in: .whitespacesAndNewlines))
+      }
+    }
+    return sentences
+  }
+}
