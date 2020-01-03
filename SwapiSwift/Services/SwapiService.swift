62c740bc73dfbb887238d759eba8534537ea631a
Joe Bramhall <joe@thinxmedia.com>
2020-01-03 07:13:40 -0500

    Filled out methods in SWAPI data service

--- SwapiSwift/Services/SwapiService.swift
+++ SwapiSwift/Services/SwapiService.swift
@@ -19,7 +19,39 @@ public protocol SwapiService {
 
   func film(withId resourceId: String) -> AnyPublisher<Film, ServiceError>
 
-  func allFilms() -> AnyPublisher<ResourceRoot<Film>, ServiceError>
+  func person(withId resourceId: String) -> AnyPublisher<Person, ServiceError>
+
+  func planet(withId resourceId: String) -> AnyPublisher<Planet, ServiceError>
+
+  func species(withId resourceId: String) -> AnyPublisher<Species, ServiceError>
+
+  func starship(withId resourceId: String) -> AnyPublisher<Starship, ServiceError>
+
+  func vehicle(withId resourceId: String) -> AnyPublisher<Vehicle, ServiceError>
+
+  func allFilms(page: String?) -> AnyPublisher<ResourceRoot<Film>, ServiceError>
+
+  func allPeople(page: String?) -> AnyPublisher<ResourceRoot<Person>, ServiceError>
+
+  func allPlanets(page: String?) -> AnyPublisher<ResourceRoot<Person>, ServiceError>
+
+  func allSpecies(page: String?) -> AnyPublisher<ResourceRoot<Species>, ServiceError>
+
+  func allStarships(page: String?) -> AnyPublisher<ResourceRoot<Starship>, ServiceError>
+
+  func allVehicles(page: String?) -> AnyPublisher<ResourceRoot<Vehicle>, ServiceError>
+
+  func people(fromResourceUrls urls: [String]) -> AnyPublisher<[Person], ServiceError>
+
+  func starships(fromResourceUrls urls: [String]) -> AnyPublisher<[Starship], ServiceError>
+
+  func planets(fromResourceUrls urls: [String]) -> AnyPublisher<[Planet], ServiceError>
+
+  func species(fromResourceUrls urls: [String]) -> AnyPublisher<[Species], ServiceError>
+
+  func vehicles(fromResourceUrls urls: [String]) -> AnyPublisher<[Vehicle], ServiceError>
+
+  func films(fromResourceUrls urls: [String]) -> AnyPublisher<[Film], ServiceError>
 
 }
 
@@ -31,15 +63,115 @@ public struct DataService: SwapiService {
 
   public func film(withId resourceId: String) -> AnyPublisher<Film, ServiceError> {
     let config = RequestConfiguration(resource: .films)
-    return swapiResource(request: Request(config: config, resourceId: resourceId))
+    return resource(request: Request(config: config, resourceId: resourceId))
   }
 
-  public func allFilms() -> AnyPublisher<ResourceRoot<Film>, ServiceError> {
-    let config = RequestConfiguration(resource: .films)
-    return swapiResource(request: Request(config: config))
+  public func person(withId resourceId: String) -> AnyPublisher<Person, ServiceError> {
+    let config = RequestConfiguration(resource: .people)
+    return resource(request: Request(config: config, resourceId: resourceId))
   }
 
-  private func swapiResource<T: Decodable>(request: Request) -> AnyPublisher<T, ServiceError> {
+  public func planet(withId resourceId: String) -> AnyPublisher<Planet, ServiceError> {
+    let config = RequestConfiguration(resource: .planets)
+    return resource(request: Request(config: config, resourceId: resourceId))
+  }
+
+  public func species(withId resourceId: String) -> AnyPublisher<Species, ServiceError> {
+    let config = RequestConfiguration(resource: .species)
+    return resource(request: Request(config: config, resourceId: resourceId))
+  }
+
+  public func starship(withId resourceId: String) -> AnyPublisher<Starship, ServiceError> {
+    let config = RequestConfiguration(resource: .starships)
+    return resource(request: Request(config: config, resourceId: resourceId))
+  }
+
+  public func vehicle(withId resourceId: String) -> AnyPublisher<Vehicle, ServiceError> {
+    let config = RequestConfiguration(resource: .vehicles)
+    return resource(request: Request(config: config, resourceId: resourceId))
+  }
+
+  public func allFilms(page: String?) -> AnyPublisher<ResourceRoot<Film>, ServiceError> {
+    guard let pageUrl = page else {
+      let config = RequestConfiguration(resource: .films)
+      return resource(request: Request(config: config))
+    }
+    return resource(request: Request(resourceUrl: pageUrl))
+  }
+
+  public func allPeople(page: String?) -> AnyPublisher<ResourceRoot<Person>, ServiceError> {
+    guard let pageUrl = page else {
+      let config = RequestConfiguration(resource: .people)
+      return resource(request: Request(config: config))
+    }
+    return resource(request: Request(resourceUrl: pageUrl))
+  }
+
+  public func allPlanets(page: String?) -> AnyPublisher<ResourceRoot<Person>, ServiceError> {
+    guard let pageUrl = page else {
+      let config = RequestConfiguration(resource: .planets)
+      return resource(request: Request(config: config))
+    }
+    return resource(request: Request(resourceUrl: pageUrl))
+  }
+
+  public func allSpecies(page: String?) -> AnyPublisher<ResourceRoot<Species>, ServiceError> {
+    guard let pageUrl = page else {
+      let config = RequestConfiguration(resource: .species)
+      return resource(request: Request(config: config))
+    }
+    return resource(request: Request(resourceUrl: pageUrl))
+  }
+
+  public func allStarships(page: String?) -> AnyPublisher<ResourceRoot<Starship>, ServiceError> {
+    guard let pageUrl = page else {
+      let config = RequestConfiguration(resource: .starships)
+      return resource(request: Request(config: config))
+    }
+    return resource(request: Request(resourceUrl: pageUrl))
+  }
+
+  public func allVehicles(page: String?) -> AnyPublisher<ResourceRoot<Vehicle>, ServiceError> {
+    guard let pageUrl = page else {
+      let config = RequestConfiguration(resource: .vehicles)
+      return resource(request: Request(config: config))
+    }
+    return resource(request: Request(resourceUrl: pageUrl))
+  }
+
+  public func people(fromResourceUrls urls: [String]) -> AnyPublisher<[Person], ServiceError> {
+    let requests = urls.map { Request(resourceUrl: $0) }
+    return resources(requests: requests)
+  }
+
+  public func starships(fromResourceUrls urls: [String]) -> AnyPublisher<[Starship], ServiceError> {
+    return resources(requests: urls.map { Request(resourceUrl: $0) })
+  }
+
+  public func planets(fromResourceUrls urls: [String]) -> AnyPublisher<[Planet], ServiceError> {
+    return resources(requests: urls.map { Request(resourceUrl: $0) })
+  }
+
+  public func species(fromResourceUrls urls: [String]) -> AnyPublisher<[Species], ServiceError> {
+    return resources(requests: urls.map { Request(resourceUrl: $0) })
+  }
+
+  public func vehicles(fromResourceUrls urls: [String]) -> AnyPublisher<[Vehicle], ServiceError> {
+    return resources(requests: urls.map { Request(resourceUrl: $0) })
+  }
+
+  public func films(fromResourceUrls urls: [String]) -> AnyPublisher<[Film], ServiceError> {
+    return resources(requests: urls.map { Request(resourceUrl: $0) })
+  }
+
+  private func resources<T: SwapiResource>(requests: [Request]) -> AnyPublisher<[T], ServiceError> {
+    let masterPublisher = Publishers.Sequence<[AnyPublisher<T, ServiceError>], ServiceError>(
+      sequence: requests.map(resource(request:))
+    )
+    return masterPublisher.flatMap { $0 }.collect().eraseToAnyPublisher()
+  }
+
+  private func resource<T: SwapiResource>(request: Request) -> AnyPublisher<T, ServiceError> {
     return dataFetcher.fetch(request.url)
       .mapError { error in
         ServiceError.networkError(error: error)
