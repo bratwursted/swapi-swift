582f2fcb5298234abe5f12757d4583f5ad8ecf5d
Joe Bramhall <joe@thinxmedia.com>
2020-01-03 07:14:25 -0500

    Updated playground

--- SwapiPlayground.playground/Contents.swift
+++ SwapiPlayground.playground/Contents.swift
@@ -6,7 +6,28 @@ var str = "Hello, playground"
 
 let service = DataService()
 
-let disposables = Set<AnyCancellable>()
+var disposables = Set<AnyCancellable>()
+
+func characters(from film: Film) {
+  let service = FilmGraphService(film: film)
+  service.fetchAssociatedProperties()
+
+  service
+    .$characters
+    .sink(receiveCompletion: { completion in
+      switch completion {
+      case .failure:
+        print("something went wrong")
+      case .finished:
+        break
+      }
+    }, receiveValue: { characters in
+      print("the film has \(characters.count) characters")
+      characters.forEach { print("\($0.name)") }
+    })
+  .store(in: &disposables)
+}
+
 
 service.film(withId: "1")
   .receive(on: DispatchQueue.main)
@@ -19,20 +40,22 @@ service.film(withId: "1")
     }
   }) { film in
     print("got the film titled \"\(film.title)\"")
+    characters(from: film)
 }
 
-service.allFilms()
-  .map { response in
-    response.results
-}
-.receive(on: DispatchQueue.main)
-.sink(receiveCompletion: { completion in
-  switch completion {
-  case .finished:
-    break
-  case .failure(let error):
-    print("There was an error \(error)")
-  }
-}) { films in
-  print("received \(films.count) films")
-}
+//service.allFilms()
+//  .map { response in
+//    response.results
+//}
+//.receive(on: DispatchQueue.main)
+//.sink(receiveCompletion: { completion in
+//  switch completion {
+//  case .finished:
+//    break
+//  case .failure(let error):
+//    print("There was an error \(error)")
+//  }
+//}) { films in
+//  print("received \(films.count) films")
+//}
+
