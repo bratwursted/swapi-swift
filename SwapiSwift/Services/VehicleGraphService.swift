23d2c599a0c778ea5d19239714e084571ab24b74
Joe Bramhall <joe@thinxmedia.com>
2020-01-03 08:06:34 -0500

    Added graph service for vehicles

---
+++ SwapiSwift/Services/VehicleGraphService.swift
@@ -0,0 +1,72 @@
+//
+//  VehicleGraphService.swift
+//  SwapiSwift
+//
+//  Created by Joe Bramhall on 1/3/20.
+//  Copyright © 2020 thinx. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+final class VehicleGraphService: GraphService {
+
+  let vehicle: Vehicle
+
+  private let dataService: SwapiService
+
+  private var disposables = Set<AnyCancellable>()
+
+  // Associated properties
+
+  @Published var films: [Film] = []
+
+  @Published var pilots: [Person] = []
+
+  init(
+    vehicle: Vehicle,
+    dataService: SwapiService = DataService()
+  ) {
+    self.vehicle = vehicle
+    self.dataService = dataService
+  }
+
+  func fetchAssociatedProperties() {
+
+    // get films
+
+    fetchFilms(
+      from: dataService,
+      with: vehicle.films,
+      receiveCompletion: { completion in
+        switch completion {
+        case .failure(let error):
+          self.handleError(error)
+          self.films = []
+        case .finished:
+          break
+        }
+    }, receiveValue: { films in
+      self.films = films
+    })
+    .store(in: &disposables)
+
+    // get pilots
+
+    fetchPeople(
+      from: dataService,
+      with: vehicle.pilots,
+      receiveCompletion: { completion in
+        switch completion {
+        case .failure(let error):
+          self.handleError(error)
+          self.pilots = []
+        case .finished:
+          break
+        }
+    }, receiveValue: { people in
+      self.pilots = people
+    })
+    .store(in: &disposables)
+  }
+}
