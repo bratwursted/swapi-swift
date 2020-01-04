3b0de9791d0bd0c1186f30f7d6cee4a25af26c0a
Joe Bramhall <joe@thinxmedia.com>
2020-01-04 17:45:35 -0500

    Added header views

---
+++ SwapiSampleApp/SwapiSampleApp/Film/FilmHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  FilmHeaderView.swift
+//  SwapiSampleApp
+//
+//  Created by Joe Bramhall on 1/4/20.
+//  Copyright © 2020 thinx. All rights reserved.
+//
+
+import SwiftUI
+
+struct FilmHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FilmHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        FilmHeaderView()
+    }
+}
---
+++ SwapiSampleApp/SwapiSampleApp/Film/FilmHeaderViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  FilmHeaderViewModel.swift
+//  SwapiSampleApp
+//
+//  Created by Joe Bramhall on 1/4/20.
+//  Copyright © 2020 thinx. All rights reserved.
+//
+
+import Foundation
