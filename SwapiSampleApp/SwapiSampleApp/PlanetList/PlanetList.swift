ab632d66ab74776c2b95124b6ebb20008911d8de
Joe Bramhall <joe@thinxmedia.com>
2020-01-05 10:24:49 -0500

    Added Planet list

---
+++ SwapiSampleApp/SwapiSampleApp/PlanetList/PlanetList.swift
@@ -0,0 +1,21 @@
+//
+//  PlanetList.swift
+//  SwapiSampleApp
+//
+//  Created by Joe Bramhall on 1/5/20.
+//  Copyright © 2020 thinx. All rights reserved.
+//
+
+import SwiftUI
+
+struct PlanetList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlanetList_Previews: PreviewProvider {
+    static var previews: some View {
+        PlanetList()
+    }
+}
---
+++ SwapiSampleApp/SwapiSampleApp/PlanetList/PlanetListViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  PlanetListViewModel.swift
+//  SwapiSampleApp
+//
+//  Created by Joe Bramhall on 1/5/20.
+//  Copyright © 2020 thinx. All rights reserved.
+//
+
+import Foundation
