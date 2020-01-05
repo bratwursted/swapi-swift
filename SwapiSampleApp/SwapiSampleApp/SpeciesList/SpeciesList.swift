330a9578f167625ca102e715ef67f84c739dd1d0
Joe Bramhall <joe@thinxmedia.com>
2020-01-05 10:34:05 -0500

    Added species list view

---
+++ SwapiSampleApp/SwapiSampleApp/SpeciesList/SpeciesList.swift
@@ -0,0 +1,21 @@
+//
+//  SpeciesList.swift
+//  SwapiSampleApp
+//
+//  Created by Joe Bramhall on 1/5/20.
+//  Copyright © 2020 thinx. All rights reserved.
+//
+
+import SwiftUI
+
+struct SpeciesList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SpeciesList_Previews: PreviewProvider {
+    static var previews: some View {
+        SpeciesList()
+    }
+}
