9325d4ea8a4617bf7456b43a9b27323f658f2710
Joe Bramhall <joe@thinxmedia.com>
2020-01-06 17:07:08 -0500

    Added Planet view and view model

---
+++ SwapiSampleApp/SwapiSampleApp/Planet/PlanetView.swift
@@ -0,0 +1,21 @@
+//
+//  PlanetView.swift
+//  SwapiSampleApp
+//
+//  Created by Joe Bramhall on 1/6/20.
+//  Copyright © 2020 thinx. All rights reserved.
+//
+
+import SwiftUI
+
+struct PlanetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlanetView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlanetView()
+    }
+}
