c8f4afbd017b3b6ddf721d23d149410ba0994b2f
Joe Bramhall <joe@thinxmedia.com>
2020-01-06 23:57:46 -0500

    Filled out species view

--- SwapiSampleApp/SwapiSampleApp/Species/SpeciesVIewModel.swift
+++ SwapiSampleApp/SwapiSampleApp/Species/SpeciesVIewModel.swift
@@ -7,3 +7,141 @@
 //
 
 import Foundation
+import SwapiSwift
+import Combine
+
+final class SpeciesViewModel: ObservableObject {
+
+  private let formatter: NumberFormatter = {
+    let formatter = NumberFormatter()
+    formatter.locale = Locale.current
+    formatter.numberStyle = .decimal
+    return formatter
+  }()
+
+  private let speciesService: SpeciesGraphService
+
+  private var species: Species {
+    speciesService.species
+  }
+
+  private var disposables = Set<AnyCancellable>()
+
+  private var needsProperties = true
+
+  @Published var films: [Film] = []
+
+  @Published var people: [Person] = []
+
+  @Published var homeworld: Planet?
+
+  init(graphService: SpeciesGraphService) {
+    speciesService = graphService
+  }
+
+  func fetchProperties() {
+    guard needsProperties else { return }
+    needsProperties = false
+
+    speciesService.fetchAssociatedProperties()
+
+    speciesService
+      .$films
+      .receive(on: DispatchQueue.main)
+      .sink { films in
+        self.films = films.sorted(by: { (film1, film2) -> Bool in
+          film1.episodeId < film2.episodeId
+        })
+    }
+    .store(in: &disposables)
+
+    speciesService
+      .$people
+      .receive(on: DispatchQueue.main)
+      .sink { people in
+        self.people = people.sorted(by: { (person1, person2) -> Bool in
+          person1.name.compare(person2.name, options: .caseInsensitive) == .orderedAscending
+        })
+    }
+    .store(in: &disposables)
+
+    speciesService
+      .$homeworld
+      .receive(on: DispatchQueue.main)
+      .sink { planet in
+        self.homeworld = planet
+    }
+    .store(in: &disposables)
+  }
+
+  var name: String {
+    species.name
+  }
+
+  var classification: String {
+    species.classification.localizedCapitalized
+  }
+
+  var designation: String {
+    species.designation.localizedCapitalized
+  }
+
+  var height: String {
+    guard species.heightMeters > 0 else {
+      return "n/a"
+    }
+    return "\(species.heightMeters) m"
+  }
+
+  var skinColors: [String] {
+    species.skinColors.split(separator: ",").map { $0.localizedCapitalized.trimmingCharacters(in: .whitespaces) }
+  }
+
+  var hairColors: [String] {
+    species.hairColors.split(separator: ",").map { $0.localizedCapitalized.trimmingCharacters(in: .whitespaces) }
+  }
+
+  var eyeColors: [String] {
+    species.eyeColors.split(separator: ",").map { $0.localizedCapitalized.trimmingCharacters(in: .whitespaces) }
+  }
+
+  var lifespan: String {
+    guard species.lifespanValue > 0 else {
+      return "n/a"
+    }
+    return formatter.string(from: NSNumber(integerLiteral: species.lifespanValue)) ?? "n/a"
+  }
+
+  var language: String {
+    species.language
+  }
+
+  var homePlanet: String {
+    homeworld?.name ?? "n/a"
+  }
+
+  func rowView(for person : Person) -> PersonRowView {
+    let vm = PersonRowViewModel(character: person)
+    return PersonRowView(viewModel: vm)
+  }
+
+  func rowView(for film: Film) -> FilmRowView {
+    let vm = FilmRowViewModel(film: film)
+    return FilmRowView(viewModel: vm)
+  }
+}
+
+extension Species {
+
+  var lifespanValue: Int {
+    Int(averageLifespan) ?? 0
+  }
+
+  var heightMeters: Float {
+    return averageHeightValue / 100
+  }
+
+  var averageHeightValue: Float {
+    Float(averageHeight) ?? 0
+  }
+}
--- SwapiSampleApp/SwapiSampleApp/Species/SpeciesView.swift
+++ SwapiSampleApp/SwapiSampleApp/Species/SpeciesView.swift
@@ -7,15 +7,127 @@
 //
 
 import SwiftUI
+import SwapiSwift
 
 struct SpeciesView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+  @ObservedObject var viewModel: SpeciesViewModel
+
+  var body: some View {
+    List {
+      infoSection
+      skinSection
+      eyesSection
+      hairSection
+      homeworldSection
+      peopleSection
+      filmsSection
     }
+    .navigationBarTitle(Text(viewModel.name), displayMode: .inline)
+    .onAppear {
+      self.viewModel.fetchProperties()
+    }
+  }
+}
+
+extension SpeciesView {
+
+  var infoSection: some View {
+    Section {
+      VStack(alignment: .leading, spacing: 12) {
+        Text(viewModel.name)
+        Text("Designation: \(viewModel.designation)")
+        Text("Classification: \(viewModel.classification)")
+        Text("Avg. height: \(viewModel.height)")
+        Text("Avg. lifespan: \(viewModel.lifespan) yrs")
+        Text("Language: \(viewModel.language)")
+      }
+    }
+  }
+
+  var skinSection: some View {
+    Section(header: Text("Skin colors")) {
+      VStack(alignment: .leading, spacing: 12) {
+        ForEach(viewModel.skinColors, id: \.self) { color in
+          Text(color)
+        }
+      }
+    }
+  }
+
+  var hairSection: some View {
+    Section(header: Text("Hair colors")) {
+      VStack(alignment: .leading, spacing: 12) {
+        ForEach(viewModel.hairColors, id: \.self) { color in
+          Text(color)
+        }
+      }
+    }
+  }
+
+  var eyesSection: some View {
+    Section(header: Text("Eye colors")) {
+      VStack(alignment: .leading, spacing: 12) {
+        ForEach(viewModel.eyeColors, id: \.self) { color in
+          Text(color)
+        }
+      }
+    }
+  }
+
+  var homeworldSection: some View {
+    Section(header: Text("Home world")) {
+      if viewModel.homeworld == nil {
+        Text("No results")
+      } else {
+        Text(viewModel.homePlanet)
+      }
+    }
+  }
+
+  var peopleSection: some View {
+    Section(header: Text("People")) {
+      if viewModel.people.isEmpty {
+        Text("No results")
+      } else {
+        ForEach(viewModel.people, id: \.self) { person in
+          self.viewModel.rowView(for: person)
+        }
+      }
+    }
+  }
+
+  var filmsSection: some View {
+    Section(header: Text("Appears in")) {
+      if viewModel.films.isEmpty {
+        Text("No results")
+      } else {
+        ForEach(viewModel.films, id: \.self) { film in
+          self.viewModel.rowView(for: film)
+        }
+      }
+    }
+  }
 }
 
 struct SpeciesView_Previews: PreviewProvider {
-    static var previews: some View {
-        SpeciesView()
+  static let vm: SpeciesViewModel = {
+    let species = sampleSpecies("hutt")
+    let planet = samplePlanet("nal-hutta")
+    let dataService = MockGraphDataService(
+      dataStore: huttData,
+      homeworld: planet
+    )
+    let graphService = SpeciesGraphService(
+      species: species,
+      dataService: dataService
+    )
+    return SpeciesViewModel(graphService: graphService)
+  }()
+
+  static var previews: some View {
+    NavigationView {
+      SpeciesView(viewModel: vm)
     }
+  }
 }
