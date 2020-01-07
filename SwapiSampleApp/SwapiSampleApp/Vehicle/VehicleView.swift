a2b12f8ff69b90069be9cae05d7f9f380404f0d6
Joe Bramhall <joe@thinxmedia.com>
2020-01-07 11:01:42 -0500

    Vehicle view

--- SwapiSampleApp/SwapiSampleApp/Vehicle/VehicleView.swift
+++ SwapiSampleApp/SwapiSampleApp/Vehicle/VehicleView.swift
@@ -7,15 +7,91 @@
 //
 
 import SwiftUI
+import SwapiSwift
 
 struct VehicleView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+  @ObservedObject var viewModel: VehicleViewModel
+
+  var body: some View {
+    List {
+      primaryInfoSection
+      capacitySection
+      pilotsSection
+      filmsSection
     }
+    .navigationBarTitle(Text(viewModel.name), displayMode: .inline)
+    .onAppear {
+      self.viewModel.fetchProperties()
+    }
+  }
+}
+
+extension VehicleView {
+
+  var primaryInfoSection: some View {
+    Section {
+      VStack(alignment: .leading, spacing: 12) {
+        Text(viewModel.name)
+        Text("Model: \(viewModel.model)")
+        Text("Class: \(viewModel.vehicleClass)")
+        Text("Made by: \(viewModel.manufacturer)")
+      }
+    }
+  }
+
+  var capacitySection: some View {
+    Section(header: Text("Size and Capacity")) {
+      VStack(alignment: .leading, spacing: 12) {
+        Text("Maximum speed: \(viewModel.maximimSpeed) kph")
+        Text("Length: \(viewModel.length) m")
+        Text("Crew: \(viewModel.crew)")
+        Text("Passengers: \(viewModel.passengers)")
+        Text("Consumables: \(viewModel.consumables)")
+        Text("Cost: \(viewModel.cost) credits")
+      }
+    }
+  }
+
+  var pilotsSection: some View {
+    Section(header: Text("Pilots")) {
+      if viewModel.pilots.isEmpty {
+        Text("No results")
+      } else {
+        ForEach(viewModel.pilots, id: \.self) { pilot in
+          self.viewModel.rowView(forPilot: pilot)
+        }
+      }
+    }
+  }
+
+  var filmsSection: some View {
+    Section(header: Text("Appears in")) {
+      if viewModel.films.isEmpty {
+        Text("No results")
+      } else {
+        ForEach(viewModel.films, id: \.self) { film in
+          self.viewModel.rowView(forFilm: film)
+        }
+      }
+    }
+  }
 }
 
 struct VehicleView_Previews: PreviewProvider {
-    static var previews: some View {
-        VehicleView()
+  static let vm: VehicleViewModel = {
+    let snowSpeeder = sampleVehicle("snowspeeder")
+    let dataService = MockGraphDataService(dataStore: snowspeederData)
+    let graphService = VehicleGraphService(
+      vehicle: snowSpeeder,
+      dataService: dataService
+    )
+    return VehicleViewModel(graphService: graphService)
+  }()
+
+  static var previews: some View {
+    NavigationView {
+      VehicleView(viewModel: vm)
     }
+  }
 }
--- SwapiSampleApp/SwapiSampleApp/Vehicle/VehicleViewModel.swift
+++ SwapiSampleApp/SwapiSampleApp/Vehicle/VehicleViewModel.swift
@@ -7,3 +7,140 @@
 //
 
 import Foundation
+import SwapiSwift
+import Combine
+
+final class VehicleViewModel: ObservableObject {
+
+  private let notAvailable = "n/a"
+
+  private let formatter: NumberFormatter = {
+    let formatter = NumberFormatter()
+    formatter.locale = Locale.current
+    formatter.numberStyle = .decimal
+    return formatter
+  }()
+
+  private let vehicleGraph: VehicleGraphService
+
+  private var vehicle: Vehicle {
+    vehicleGraph.vehicle
+  }
+
+  private var needsProperties = true
+
+  private var disposables = Set<AnyCancellable>()
+
+  @Published var films: [Film] = []
+
+  @Published var pilots: [Person] = []
+
+  init(graphService: VehicleGraphService) {
+    vehicleGraph = graphService
+  }
+
+  func fetchProperties() {
+    guard needsProperties else { return }
+    needsProperties = false
+
+    vehicleGraph.fetchAssociatedProperties()
+
+    vehicleGraph
+      .$films
+      .receive(on: DispatchQueue.main)
+      .sink { films in
+        self.films = films.sorted(by: { (film1, film2) -> Bool in
+          film1.episodeId < film2.episodeId
+        })
+    }
+    .store(in: &disposables)
+
+    vehicleGraph
+      .$pilots
+      .receive(on: DispatchQueue.main)
+      .sink { people in
+        self.pilots = people.sorted(by: { (person1, person2) -> Bool in
+          person1.name.compare(person2.name, options: .caseInsensitive) == .orderedAscending
+        })
+    }
+    .store(in: &disposables)
+  }
+
+  var name: String {
+    vehicle.name
+  }
+
+  var model: String {
+    vehicle.model
+  }
+
+  var manufacturer: String {
+    vehicle.manufacturer
+  }
+
+  var cost: String {
+    guard vehicle.costValue > 0 else {
+      return notAvailable
+    }
+    return formatter.string(from: NSNumber(integerLiteral: vehicle.costValue)) ?? notAvailable
+  }
+
+  var length: String {
+    vehicle.length
+  }
+
+  var maximimSpeed: String {
+    guard vehicle.maxSpeedValue > 0 else {
+      return notAvailable
+    }
+    return formatter.string(from: NSNumber(integerLiteral: vehicle.maxSpeedValue)) ?? notAvailable
+  }
+
+  var crew: String {
+    vehicle.crew
+  }
+
+  var passengers: String {
+    vehicle.passengers
+  }
+
+  var cargo: String {
+    guard vehicle.cargoCapacityValue > 0 else {
+      return notAvailable
+    }
+    return formatter.string(from: NSNumber(integerLiteral: vehicle.cargoCapacityValue)) ?? notAvailable
+  }
+
+  var consumables: String {
+    vehicle.consumables
+  }
+
+  var vehicleClass: String {
+    vehicle.vehicleClass.localizedCapitalized
+  }
+
+  func rowView(forPilot pilot: Person) -> PersonRowView {
+    let vm = PersonRowViewModel(character: pilot)
+    return PersonRowView(viewModel: vm)
+  }
+
+  func rowView(forFilm film: Film) -> FilmRowView {
+    let vm = FilmRowViewModel(film: film)
+    return FilmRowView(viewModel: vm)
+  }
+}
+
+extension Vehicle {
+
+  var cargoCapacityValue: Int {
+    Int(cargoCapacity) ?? 0
+  }
+
+  var costValue: Int {
+    Int(cost) ?? 0
+  }
+
+  var maxSpeedValue: Int {
+    Int(maximumAtmosphericSpeed) ?? 0
+  }
+}
