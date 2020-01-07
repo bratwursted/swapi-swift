741be930bd573d3bcb8669f89ce731aedaf4ecf3
Joe Bramhall <joe@thinxmedia.com>
2020-01-07 10:01:00 -0500

    Added vehicle view

---
+++ SwapiSampleApp/SwapiSampleApp/Vehicle/VehicleView.swift
@@ -0,0 +1,21 @@
+//
+//  VehicleView.swift
+//  SwapiSampleApp
+//
+//  Created by Joe Bramhall on 1/7/20.
+//  Copyright © 2020 thinx. All rights reserved.
+//
+
+import SwiftUI
+
+struct VehicleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VehicleView_Previews: PreviewProvider {
+    static var previews: some View {
+        VehicleView()
+    }
+}
---
+++ SwapiSampleApp/SwapiSampleApp/Vehicle/VehicleViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  VehicleViewModel.swift
+//  SwapiSampleApp
+//
+//  Created by Joe Bramhall on 1/7/20.
+//  Copyright © 2020 thinx. All rights reserved.
+//
+
+import Foundation
